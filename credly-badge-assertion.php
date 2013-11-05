90d73eb412d6bb115f2b9105faf0f1b6d950bcc6
Brian Richards <brian@rzen.net>
2013-11-04 20:30:04 -0500

    Update to reflect alterations made in BadgeOS

--- credly-badge-assertion.php
+++ credly-badge-assertion.php
@@ -2,7 +2,7 @@
 /**
 * Plugin Name: Credly Custom Badge Assertion Shortcode
 * Plugin URI: http://www.badgeos.org/
-* Description: For Credly Pro members: easily create a page on your site for displaying Credly Badge Assertions. Just add [credly_assertion_page] to whichver page you'd like to use.
+* Description: For Credly Pro members: easily create a page on your site for displaying Credly "Custom Assertion Location". Just add [credly_assertion_page] to whichver page you'd like to use. Copy that page's URL and append "?CID={id}" to the end (e.g. http://yoursite.com/assertion/?CID={id}). Paste this full URL in the "Custom Assertion Location" field in your Credly Account Settings.
 * Author: Credly
 * Version: 1.0.0
 * Author URI: https://credly.com/
@@ -30,14 +30,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>;.
  *
  * @since  1.0.0
  *
- * @return string iframe displaying Credly data, or nothing.
+ * @return` string iframe displaying Credly data, or nothing.
  */
-function credly_assertion_page_shortcode( $atts = array() ) {
+function badgeos_credly_assertion_page( $atts = array() ) {
 	global $content_width;
 
 	// Setup defaults
 	$defaults = array(
-		'ID'     => isset( $_GET['ID'] ) ? absint( $_GET['ID'] ) : 0,
+		'CID'     => isset( $_GET['CID'] ) ? absint( $_GET['CID'] ) : 0,
 		'width'  => isset( $content_width ) ? $content_width : 560,
 		'height' => 1000,
 	);
@@ -46,10 +46,10 @@ function credly_assertion_page_shortcode( $atts = array() ) {
 	$atts = shortcode_atts( $defaults, $atts );
 
 	// If passed an ID, render the iframe, otherwise render nothing
-	if ( $atts['ID'] )
-		return '<iframe class="credly-assertion" src="http://credly.com/credit/' . $atts['ID'] . '/embed" align="top" marginwidth="0" width="' . $atts['width'] . 'px" height="' . $atts['height'] . 'px" scrolling="no" frameborder="no"></iframe>';
+	if ( $atts['CID'] )
+		return '<iframe class="credly-assertion" src="http://credly.com/credit/' . $atts['CID'] . '/embed" align="top" marginwidth="0" width="' . $atts['width'] . 'px" height="' . $atts['height'] . 'px" scrolling="no" frameborder="no"></iframe>';
 	else
 		return '';
 
 }
-add_shortcode( 'credly_assertion_page', 'credly_assertion_page_shortcode' );
+add_shortcode( 'credly_assertion_page', 'badgeos_credly_assertion_page' );
